ed2bb26bfb4a79e0875d5e5f868b6d589aee2422
Stephen Blatti <stephen.p.blatti@gmail.com>
2017-11-19 08:57:36 -0600

    Add Solution

--- BS09_SUBS/BS09_SUBS.py
+++ BS09_SUBS/BS09_SUBS.py
@@ -4,3 +4,22 @@ Given: Two DNA strings s and t (each of length at most 1 kbp).
 
 Return: All locations of t as a substring of s.
 '''
+
+
+def findingMotifs(s,t):
+    inds = []
+    for i in range(len(s)):
+        if s.startswith(t, i):
+            inds.append(i+1)
+
+    return ' '.join(map(str, inds))
+
+def main():
+    filename = '../data/rosalind_subs.txt'
+    with open(filename) as f:
+        s, t = f.read().strip().split('\n')
+        print(findingMotifs(s,t))
+
+
+if __name__ == '__main__':
+    main()
